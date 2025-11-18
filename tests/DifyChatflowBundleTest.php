4f5e3d2bfb5d49ea3a85e1c03cad2bc85a9256f3
github-ci <you@example.com>
2025-11-18 23:20:56 +0800

    fix error

--- tests/DifyChatflowBundleTest.php
+++ tests/DifyChatflowBundleTest.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 namespace Tourze\DifyChatflowBundle\Tests;
 
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use HttpClientBundle\HttpClientBundle;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 use Tourze\DifyChatflowBundle\DifyChatflowBundle;
-use Tourze\DifyCoreBundle\DifyCoreBundle;
 use Tourze\PHPUnitSymfonyKernelTest\AbstractBundleTestCase;
 
 /**
@@ -19,17 +16,4 @@ use Tourze\PHPUnitSymfonyKernelTest\AbstractBundleTestCase;
 #[RunTestsInSeparateProcesses]
 final class DifyChatflowBundleTest extends AbstractBundleTestCase
 {
-    public function testGetBundleDependencies(): void
-    {
-        $dependencies = DifyChatflowBundle::getBundleDependencies();
-
-        self::assertIsArray($dependencies);
-        self::assertArrayHasKey(DoctrineBundle::class, $dependencies);
-        self::assertArrayHasKey(DifyCoreBundle::class, $dependencies);
-        self::assertArrayHasKey(HttpClientBundle::class, $dependencies);
-
-        self::assertSame(['all' => true], $dependencies[DoctrineBundle::class]);
-        self::assertSame(['all' => true], $dependencies[DifyCoreBundle::class]);
-        self::assertSame(['all' => true], $dependencies[HttpClientBundle::class]);
-    }
 }
